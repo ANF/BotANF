dd74ec83bd53862818456ac870975602ccd910fa
ANF-Studios <theanfstudios@gmail.com>
2020-12-29 12:01:31 -0500

    Fixed import directory in base files

--- src/utils/structures/BaseEvent.ts
+++ src/utils/structures/BaseEvent.ts
@@ -1,5 +1,5 @@
 
-import DiscordClient from '../../client/client';
+import DiscordClient from '../client/client';
 
 export default abstract class BaseEvent {
   constructor(private name: string) { }
