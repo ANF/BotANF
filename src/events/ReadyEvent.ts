b579183983fd654769d2d5d74981e9311761ebe9
ANF-Studios <theanfstudios@gmail.com>
2020-12-29 12:05:57 -0500

    Fixed import directory

--- src/events/ReadyEvent.ts
+++ src/events/ReadyEvent.ts
@@ -1,5 +1,5 @@
 import BaseEvent from '../utils/structures/BaseEvent';
-import DiscordClient from '../client/client';
+import DiscordClient from '../utils/client/client';
 
 export default class ReadyEvent extends BaseEvent {
   constructor() {
