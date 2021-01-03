ec952ce13272562612e1eb3727d9f5772c85a3fb
ANF-Studios <theanfstudios@gmail.com>
2021-01-03 04:26:52 -0500

    Improved startup and settings
    - Used custom loger in catch block for the main method.
    - Used settings coming from MongoDB, primarily.
    - Added more settings while creating Discord Client.
    Moved getInitialSettings to the bottom.

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,40 @@
 import { registerCommands, registerEvents } from './utils/registry';
-import DiscordClient from './utils/client/client';
+import logger, { logLevel } from './utils/helper/logger';
 import clientConfig from './utils/config/clientConfig';
-//import {  } from "./commands";
+import DiscordClient from './utils/client/client';
+import { getSettings } from './utils/client/mongodb'
 import 'dotenv/config';
 export const client = new DiscordClient({
   disableMentions: 'everyone',
-  ws: { compress: false }
+  ws: { compress: false },
+  retryLimit: 10,
 });
 
+let settings: any;
+
 async function main(configuration: clientConfig) {
-  client.prefix = process.env.PREFIX || client.prefix;
+  client.prefix = settings?.Prefix || process.env.PREFIX || client.prefix;
   await registerCommands(client, '../commands');
   await registerEvents(client, '../events');
-  await client.login(process.env.TOKEN);
+  await client.login(settings?.Token || process.env.TOKEN);
   await client.user?.setPresence({
     activity: { name: configuration.user_presence, type: configuration.user_presenceType },
     status: configuration.user_status
   });
 }
 
+async function initialize() {
+  settings = await getSettings(logger);
+  logger.log('The application is now alive and got settings from MongoDB.', logLevel.verbose, 'index');
+}
+
+initialize();
 main({
   user_status: 'idle',
   user_presence: 'as Illuminati',
   user_presenceType: 'PLAYING'
-}).catch(reason => {
-  console.log('[ERROR] Something went wrong!\n' + reason);
+}).catch(async (err) => {
+  await logger.log('There is an error in the main method; ' + err, logLevel.warning, 'index');
 });
+
+export function getInitialSettings() { return settings; }
