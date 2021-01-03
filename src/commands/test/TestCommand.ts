8c8e80891a7db288d342d90807e03bdf0ddb7fbb
ANF-Studios <theanfstudios@gmail.com>
2021-01-03 04:24:22 -0500

    Enhanced test command

--- src/commands/test/TestCommand.ts
+++ src/commands/test/TestCommand.ts
@@ -1,13 +1,17 @@
 import { Message } from 'discord.js';
 import BaseCommand from '../../utils/BaseCommand';
 import DiscordClient from '../../utils/client/client';
+import { getTextFromMongoDB } from '../../utils/client/mongodb'
+import logger from '../../utils/helper/logger';
 
 export default class TestCommand extends BaseCommand {
+  text: any = null;
   constructor() {
     super('test', 'testing', []);
   }
 
   async run(client: DiscordClient, message: Message, args: Array<string>) {
-    message.channel.send('Test command works');
+    this.text ??= await getTextFromMongoDB(logger);
+    message.channel.send(this.text?.get);
   }
 }
