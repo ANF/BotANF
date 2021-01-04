c84f09a768ef4e1a50934b536a0d6e94a10c9fa3
ANF-Studios <68814933+ANF-Studios@users.noreply.github.com>
2021-01-05 03:32:31 +0500

    Revert "Add discord client's config options"
    This reverts commit fd7480925a83f798f1d1153d36c1c64f881e3cd9.

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,15 @@
 import { registerCommands, registerEvents } from './utils/registry';
 import DiscordClient from './utils/client/client';
-import clientConfig from './utils/config/clientConfig';
 import 'dotenv/config';
-export const client = new DiscordClient({
-  disableMentions: 'everyone',
-  ws: { compress: true }
-});
+const client = new DiscordClient({});
 
-async function main(configuration: clientConfig) {
+async function main() {
   client.prefix = process.env.PREFIX || client.prefix;
   await registerCommands(client, '../commands');
   await registerEvents(client, '../events');
   await client.login(process.env.TOKEN);
-  await client.user?.setPresence({
-    activity: { name: configuration.user_presence, type: configuration.user_presenceType },
-    status: configuration.user_status
-  });
 }
 
-main({
-  user_status: 'idle',
-  user_presence: 'as Illuminati',
-  user_presenceType: 'PLAYING'
-}).catch(reason => {
+main().catch(reason => {
   console.log('[ERROR] Something went wrong!\n' + reason);
 });
