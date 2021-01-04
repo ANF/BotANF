2d8f208f51fe022f57b15ee439797cd6887c877e
ANF-Studios <68814933+ANF-Studios@users.noreply.github.com>
2021-01-05 03:32:31 +0500

    Revert "Added unknown commands handler (TODO)"
    This reverts commit 69ea33f4b92cefc57395f8c3ae044d7187c91c91.

--- src/utils/helper/UnknownCommand.ts
+++
@@ -1,4 +0,0 @@
-// TODO: Add a handler method which takes arguments
-// and sends and embed in response according to the
-// error. This will be helpful to prevent code
-// duplication and help meet our standards.
