20382d0a0e896cf0142430df017674a940cc2d52
ANF-Studios <68814933+ANF-Studios@users.noreply.github.com>
2021-01-05 03:32:31 +0500

    Revert "Created interface: clientConfig"
    This reverts commit 3312bcbd3b1a284235bdbb7f65fd1538d45c4594.

--- src/utils/config/clientConfig.ts
+++
@@ -1,7 +0,0 @@
-import { PresenceStatusData, ActivityType } from 'discord.js';
-
-export default interface clientConfig {
-    user_status: PresenceStatusData,
-    user_presence: string,
-    user_presenceType: ActivityType;
-}
