5590649e721531b6f66921ad504ed32d8bbc864a
ANF-Studios <theanfstudios@gmail.com>
2021-01-04 16:05:13 -0500

    Added rules command

---
+++ src/commands/other/RulesCommand.ts
@@ -0,0 +1,16 @@
+import { Message } from 'discord.js';
+import BaseCommand from '../../utils/BaseCommand';
+import DiscordClient from '../../utils/client/client';
+import mongoSettings from '../../utils/config/MongoSettings';
+
+export default class RulesCommand extends BaseCommand {
+    constructor() {
+        super('rule', 'Other', []);
+    }
+
+    async run(client: DiscordClient, message: Message, args: Array<string>) {
+        if (mongoSettings.Rules[Number(args[0]) - 1]) message.channel.send(`Rule ${args[0]}\n${mongoSettings.Rules[Number(args[0]) - 1]}`);
+        else message.channel.send('err, invalid argument');
+        
+    }
+}
