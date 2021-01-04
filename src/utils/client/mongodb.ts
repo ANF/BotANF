6a8e11275b61fb273d7d3c4978459f07590cebc3
ANF-Studios <68814933+ANF-Studios@users.noreply.github.com>
2021-01-05 03:32:31 +0500

    Revert "Expanded mongodb; added getSettings and getText functions"
    This reverts commit 9137a9b8156d71c2670461b2d82329ffb3f3908e.

--- src/utils/client/mongodb.ts
+++ src/utils/client/mongodb.ts
@@ -1,31 +1,11 @@
 import { MongoClient, MongoError } from 'mongodb';
-import 'dotenv/config';
-import { logLevel, Logger } from '../helper/logger';
 
 export const mongoClient = new MongoClient(`${process.env.MONGO}`, { useNewUrlParser: true, useUnifiedTopology: true });
-const databaseName = "Bot-DB";
-
-export async function getSettings(_logger: Logger) {
-    return new Promise((resolve, reject) => {
-        mongoClient?.connect((err: MongoError, client: MongoClient) => {
-            if (err) _logger.log('Something went wrong while trying to connect to the database.', logLevel.warning, 'mongodb');
-            const collection = client.db(databaseName, { noListener: true, returnNonCachedInstance: true }).collection("Config");
-            collection.findOne({ name: "Config" }, (err: MongoError, result: any) => {
-                if (err) _logger.log('Something went wrong while trying to get the configuration from the collection.', logLevel.warning, 'mongodb');
-                resolve(result);
-            });
-        });
-    })
-}
-
-export async function getTextFromMongoDB(_logger: Logger) {
-    return new Promise((resolve, reject) => {
-        mongoClient?.connect((err: MongoError, client: MongoClient) => {
-            const collection = client.db(databaseName, { returnNonCachedInstance: false }).collection("test");
-            collection.findOne({ name: "delivery" }, (err: MongoError, result: any) => {
-                if (err) _logger.log('Something went wrong while trying to get the configuration from the collection.', logLevel.warning, 'mongodb');
-                resolve(result);
-            });
-        });
+mongoClient.connect((err: MongoError) => {
+    if (err) console.log(err);
+    const collection = mongoClient.db("Bot-DB").collection("test");
+    collection.findOne({ name: "delivery" }, (err: MongoError, result: any) => {
+        if (err) console.log(err);
+        console.log(result);
     });
-}
+});
