39dcb824b66820140685c97781ee45f8bd9f58bf
ANF-Studios <theanfstudios@gmail.com>
2021-01-04 16:04:13 -0500

    Changed clientConfig to PascalCase

--- src/utils/config/clientConfig.ts
+++ src/utils/config/clientConfig.ts
@@ -1,6 +1,6 @@
 import { PresenceStatusData, ActivityType } from 'discord.js';
 
-export default interface clientConfig {
+export default interface ClientConfig {
     user_status: PresenceStatusData,
     user_presence: string,
     user_presenceType: ActivityType;
