5434b251a31aa10417d6be9720f4ec47cf665d51
ANF-Studios <68814933+ANF-Studios@users.noreply.github.com>
2021-01-05 03:32:31 +0500

    Revert "Readded ClientConfig"
    This reverts commit 8669016e093841f71f92786db67656667bf3f5a3.

--- src/utils/config/ClientConfig.ts
+++
@@ -1,7 +0,0 @@
-import { PresenceStatusData, ActivityType } from 'discord.js';
-
-export default interface ClientConfig {
-    user_status: PresenceStatusData,
-    user_presence: string,
-    user_presenceType: ActivityType;
-}
