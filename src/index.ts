27ee8decbd16b4cd27d7a449a4fc518a335beb58
ANF-Studios <68814933+ANF-Studios@users.noreply.github.com>
2021-01-05 03:32:31 +0500

    Revert "Improved startup and settings"
    This reverts commit 33a8f82fa884ef80dc90444b7b406acfdb388eac.

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,28 @@
 import { registerCommands, registerEvents } from './utils/registry';
-import logger, { logLevel } from './utils/helper/logger';
-import clientConfig from './utils/config/clientConfig';
 import DiscordClient from './utils/client/client';
-import { getSettings } from './utils/client/mongodb'
+import clientConfig from './utils/config/clientConfig';
+//import {  } from "./commands";
 import 'dotenv/config';
 export const client = new DiscordClient({
   disableMentions: 'everyone',
-  ws: { compress: false },
-  retryLimit: 10,
+  ws: { compress: false }
 });
 
-let settings: any;
-
 async function main(configuration: clientConfig) {
-  client.prefix = settings?.Prefix || process.env.PREFIX || client.prefix;
+  client.prefix = process.env.PREFIX || client.prefix;
   await registerCommands(client, '../commands');
   await registerEvents(client, '../events');
-  await client.login(settings?.Token || process.env.TOKEN);
+  await client.login(process.env.TOKEN);
   await client.user?.setPresence({
     activity: { name: configuration.user_presence, type: configuration.user_presenceType },
     status: configuration.user_status
   });
 }
 
-async function initialize() {
-  settings = await getSettings(logger);
-  logger.log('The application is now alive and got settings from MongoDB.', logLevel.verbose, 'index');
-}
-
-initialize();
 main({
   user_status: 'idle',
   user_presence: 'as Illuminati',
   user_presenceType: 'PLAYING'
-}).catch(async (err) => {
-  await logger.log('There is an error in the main method; ' + err, logLevel.warning, 'index');
+}).catch(reason => {
+  console.log('[ERROR] Something went wrong!\n' + reason);
 });
-
-export function getInitialSettings() { return settings; }
