2b01451b3a8c9f80f4206d4b3872fba1a6da6d61
ANF-Studios <68814933+ANF-Studios@users.noreply.github.com>
2021-01-05 03:32:31 +0500

    Revert "Delete clientConfig.ts"
    This reverts commit 1410da749c7eef3b525093c4b85cd889ce52648d.

---
+++ src/utils/config/clientConfig.ts
@@ -0,0 +1,7 @@
+import { PresenceStatusData, ActivityType } from 'discord.js';
+
+export default interface ClientConfig {
+    user_status: PresenceStatusData,
+    user_presence: string,
+    user_presenceType: ActivityType;
+}
