69ea33f4b92cefc57395f8c3ae044d7187c91c91
ANF-Studios <theanfstudios@gmail.com>
2021-01-04 16:04:49 -0500

    Added unknown commands handler (TODO)

---
+++ src/utils/helper/UnknownCommand.ts
@@ -0,0 +1,4 @@
+// TODO: Add a handler method which takes arguments
+// and sends and embed in response according to the
+// error. This will be helpful to prevent code
+// duplication and help meet our standards.
