ebf35bf15c57cfba553f8b1e0db1b95324d5679b
ANF-Studios <68814933+ANF-Studios@users.noreply.github.com>
2021-01-05 03:32:31 +0500

    Revert "Improved client and mongo connection"
    This reverts commit d5aa0e45dee5b6ea4ba9d7c7698086871abb9208.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
 import { registerCommands, registerEvents } from './utils/registry';
 import logger, { logLevel } from './utils/helper/logger';
-import ClientConfig from './utils/config/ClientConfig';
+import clientConfig from './utils/config/clientConfig';
 import DiscordClient from './utils/client/client';
-import { getSettings } from './utils/client/mongodb';
-import MongoSettings from './utils/config/MongoSettings';
+import { getSettings } from './utils/client/mongodb'
 import 'dotenv/config';
 export const client = new DiscordClient({
   disableMentions: 'everyone',
@@ -11,11 +10,13 @@ export const client = new DiscordClient({
   retryLimit: 10,
 });
 
-async function main(configuration: ClientConfig): Promise<void> {
-  client.prefix = MongoSettings?.Prefix || process.env.PREFIX || client.prefix;
+let settings: any;
+
+async function main(configuration: clientConfig) {
+  client.prefix = settings?.Prefix || process.env.PREFIX || client.prefix;
   await registerCommands(client, '../commands');
   await registerEvents(client, '../events');
-  await client.login(MongoSettings?.Token || process.env.TOKEN);
+  await client.login(settings?.Token || process.env.TOKEN);
   await client.user?.setPresence({
     activity: { name: configuration.user_presence, type: configuration.user_presenceType },
     status: configuration.user_status
@@ -23,11 +24,7 @@ async function main(configuration: ClientConfig): Promise<void> {
 }
 
 async function initialize() {
-  const settings: any = await getSettings(logger);
-  MongoSettings.Token = settings.Token;
-  MongoSettings.Version = settings.Version;
-  MongoSettings.Prefix = settings.Prefix;
-  MongoSettings.Rules = settings.Rules;
+  settings = await getSettings(logger);
   logger.log('The application is now alive and got settings from MongoDB.', logLevel.verbose, 'index');
 }
 
@@ -37,5 +34,7 @@ main({
   user_presence: 'as Illuminati',
   user_presenceType: 'PLAYING'
 }).catch(async (err) => {
-  logger.log('There is an error in the main method; ' + err, logLevel.warning, 'index');
+  await logger.log('There is an error in the main method; ' + err, logLevel.warning, 'index');
 });
+
+export function getInitialSettings() { return settings; }
