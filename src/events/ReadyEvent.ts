f9e63196d8e88beceaa64bdfbb64ea781b5535b1
ANF-Studios <68814933+ANF-Studios@users.noreply.github.com>
2021-01-05 03:32:31 +0500

    Revert "Replaced logger"
    This reverts commit f555df66353ec4cdd3c9eccfdd0c6e37ceee1b8b.

--- src/events/ReadyEvent.ts
+++ src/events/ReadyEvent.ts
@@ -1,12 +1,11 @@
 import BaseEvent from '../utils/BaseEvent';
 import DiscordClient from '../utils/client/client';
-import logger, { logLevel } from '../utils/helper/logger';
 
 export default class ReadyEvent extends BaseEvent {
   constructor() {
     super('ready');
   }
   async run (client: DiscordClient) {
-    logger.log('The bot; ' + client.user?.username + ' has successfully started and is now functional.', logLevel.info, 'ReadyEvent');
+    console.log('Bot has logged in.');
   }
 }
