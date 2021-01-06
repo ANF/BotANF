d07f2eec6596d20e06bb68421f33d1b57ff49821
ANF-Studios <theanfstudios@gmail.com>
2021-01-06 08:16:23 -0500

    Formatted Say Command

--- src/commands/other/SayCommand.ts
+++ src/commands/other/SayCommand.ts
@@ -1,15 +1,15 @@
-import { Message } from 'discord.js';
-import { client } from '../..';
-import BaseCommand from '../../utils/BaseCommand';
-import DiscordClient from '../../utils/client/client';
+import { Message } from "discord.js";
+import { client } from "../..";
+import BaseCommand from "../../utils/BaseCommand";
+import DiscordClient from "../../utils/client/client";
 
-export default class PingCommand extends BaseCommand {
+export default class SayCommand extends BaseCommand {
   constructor() {
-    super('say', 'Says whatever the user says.', [client.prefix]);
+    super("say", "Utility", "Says whatever the user says.", [client.prefix]);
   }
 
   // TODO: Fix a bug where new line doesn't work in sent messages.
   async run(client: DiscordClient, message: Message, args: Array<string>) {
-    message.channel.send(args.join(' '));
+    message.channel.send(args.join(" "));
   }
 }
