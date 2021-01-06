41ad33b9c40fe0a930254d5a86b94b6c9150193f
ANF-Studios <theanfstudios@gmail.com>
2021-01-06 08:18:54 -0500

    Formatted Base Event

--- src/utils/BaseEvent.ts
+++ src/utils/BaseEvent.ts
@@ -1,8 +1,10 @@
-import DiscordClient from './client/client';
+import DiscordClient from "./client/client";
 
 export default abstract class BaseEvent {
-  constructor(private name: string) { }
+  constructor(private name: string) {}
 
-  getName(): string { return this.name; }
+  getName(): string {
+    return this.name;
+  }
   abstract run(client: DiscordClient, ...args: any): void;
 }
