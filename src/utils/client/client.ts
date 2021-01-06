b66e574df0921993f5a52033a65a498b493c3648
ANF-Studios <theanfstudios@gmail.com>
2021-01-06 08:18:44 -0500

    Formatted Discord Client file

--- src/utils/client/client.ts
+++ src/utils/client/client.ts
@@ -1,19 +1,27 @@
-import { Client, ClientOptions, Collection } from 'discord.js';
-import BaseEvent from '../BaseEvent';
-import BaseCommand from '../BaseCommand';
+import { Client, ClientOptions, Collection } from "discord.js";
+import BaseEvent from "../BaseEvent";
+import BaseCommand from "../BaseCommand";
 
 export default class DiscordClient extends Client {
   private _commands = new Collection<string, BaseCommand>();
   private _events = new Collection<string, BaseEvent>();
-  private _prefix: string = ','; // The original BotANF™ prefix.
+  private _prefix: string = ","; // The original BotANF™ prefix.
 
   constructor(options?: ClientOptions) {
     super(options);
   }
 
-  get commands(): Collection<string, BaseCommand> { return this._commands; }
-  get events(): Collection<string, BaseEvent> { return this._events; }
-  get prefix(): string { return this._prefix; }
+  get commands(): Collection<string, BaseCommand> {
+    return this._commands;
+  }
+  get events(): Collection<string, BaseEvent> {
+    return this._events;
+  }
+  get prefix(): string {
+    return this._prefix;
+  }
 
-  set prefix(prefix: string) { this._prefix = prefix; }
+  set prefix(prefix: string) {
+    this._prefix = prefix;
+  }
 }
