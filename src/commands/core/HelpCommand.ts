0bfc93e7f8d0827cee52c51296085a9dd1e0a8f2
ANF-Studios <theanfstudios@gmail.com>
2021-01-06 16:23:24 -0500

    Added commands in embed

--- src/commands/core/HelpCommand.ts
+++ src/commands/core/HelpCommand.ts
@@ -13,6 +13,14 @@ export default class HelpCommand extends BaseCommand {
     const helpEmbed: MessageEmbed = new MessageEmbed({
       color: EmbedColor.NULL,
     });
+    BaseCommand.commandNames.forEach((value, index) => {
+      // This part is fine but as more commands get added,
+      // the embed amount needs to be limited, perhaps
+      // somewhere around 5 commands per embed and
+      // I think we should do a reaction embed
+      // as it has been requested a lot of times.
+      helpEmbed.addField(value, BaseCommand.commandDescriptions[index], false);
+    });
     message.channel.send(helpEmbed);
   }
 }
