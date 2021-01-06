42fb6a0a2c701ca868272c29d539a192c71145b8
ANF-Studios <theanfstudios@gmail.com>
2021-01-06 08:18:17 -0500

    Formatted Client settings

--- src/utils/config/ClientConfig.ts
+++ src/utils/config/ClientConfig.ts
@@ -1,7 +1,7 @@
-import { PresenceStatusData, ActivityType } from 'discord.js';
+import { PresenceStatusData, ActivityType } from "discord.js";
 
 export default interface ClientConfig {
-    user_status: PresenceStatusData,
-    user_presence: string,
-    user_presenceType: ActivityType;
+  user_status: PresenceStatusData;
+  user_presence: string;
+  user_presenceType: ActivityType;
 }
