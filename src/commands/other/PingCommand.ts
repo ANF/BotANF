43e0fcecbd49dd6da263d092527e8f458b6d2ab2
ANF-Studios <theanfstudios@gmail.com>
2021-01-06 08:16:02 -0500

    Formatted ping command

--- src/commands/other/PingCommand.ts
+++ src/commands/other/PingCommand.ts
@@ -1,13 +1,21 @@
-import { Message } from 'discord.js';
-import BaseCommand from '../../utils/BaseCommand';
-import DiscordClient from '../../utils/client/client';
+import { Message } from "discord.js";
+import BaseCommand from "../../utils/BaseCommand";
+import DiscordClient from "../../utils/client/client";
 
 export default class PingCommand extends BaseCommand {
   constructor() {
-    super('ping', 'Utility', ['latency', 'pong']);
+    super("ping", "Utility", "Displays the current latency of the bot.", [
+      "latency",
+      "pong",
+    ]);
   }
 
   async run(client: DiscordClient, message: Message, args: Array<string>) {
-    message.channel.send(`The current latency is \`${client.ws.ping}\`ms`);
+    message.channel.send({
+      embed: {
+        color: 3092790,
+        description: `The current latency is \`${client.ws.ping}\`ms`,
+      },
+    });
   }
 }
