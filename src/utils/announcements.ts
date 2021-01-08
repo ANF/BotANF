94e0c1da6fc66dff45c15e4803b7458b4e29f442
Billy <70792552+billyeatcookies@users.noreply.github.com>
2021-01-08 17:56:18 +0530

    created announcements.ts
    created announcements.ts script, this is for returning result to user after using a command.

---
+++ src/utils/announcements.ts
@@ -0,0 +1,46 @@
+import * as Discord from "discord.js"
+
+enum Colors {
+    TRANSPARENT = 0x36393f,
+    BAD_RED = 0xff7675,
+    GOOD_GREEN = 0x55efc4,
+    WARNING_YELLOW = 0xfdcb6e,
+    INFO_LIGHTBLUE = 0x74b9ff
+}
+
+export class Announcements {
+
+    public static error(discordMessageInstance: Discord.Message, title: string, description?: string, deleteAfter?: boolean) {
+        const embed = new Discord.RichEmbed().setColor(Colors.BAD_RED).setTitle(`${title}`);
+
+        if (description) {
+            embed.setDescription(description);
+        }
+
+        const sentMessage = discordMessageInstance.channel.send(embed);
+
+        if (deleteAfter) {
+            sentMessage.then(message => {
+                // @ts-ignore
+                message.delete(5000);
+            });
+        }
+    }
+
+    public static success(discordMessageInstance: Discord.Message, title: string, description?: string, deleteAfter?: boolean) {
+        const embed = new Discord.RichEmbed().setColor(Colors.GOOD_GREEN).setTitle(`✔️ ${title}`);
+
+        if (description) {
+            embed.setDescription(description);
+        }
+
+        const sentMessage = discordMessageInstance.channel.send(embed);
+
+        if (deleteAfter) {
+            sentMessage.then(message => {
+                // @ts-ignore
+                message.delete(5000);
+            });
+        }
+    }
+}
