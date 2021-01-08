3f12c869091aa1a009826534501e88d369e219ff
Billy <70792552+billyeatcookies@users.noreply.github.com>
2021-01-08 18:33:40 +0530

    added argchecks
    argchecks is for checking arguments

--- src/utils/checks.ts
+++ src/utils/checks.ts
@@ -3,12 +3,23 @@ import BaseCommand from './BaseCommand';
 import {Announcements} from "./announcements";
 
 export class Checks {
+    // permission check system
     public static permissionCheck(discordMessageInstance: Message, permission: PermissionResolvable): boolean {
         if (!discordMessageInstance.member.hasPermission(permission)) {
             Announcements.error(discordMessageInstance, "You dont have permissions for that", undefined, true);
             return false;
         }
 
+        return true;
+    }
+    
+    // arguments check system
+    public static argsCheck(discordMessageInstance: Discord.Message, commandInstance: ICommand, args: string[]): boolean {
+        const splitArgs = commandInstance.args.split("[");
+        if (args.length < splitArgs.length) {
+            Announcements.error(discordMessageInstance, "Invalid usage", `Proper: **${commandInstance.syntax} ${commandInstance.args}**`, false);
+            return false;
+        }
         return true;
     }
 }
