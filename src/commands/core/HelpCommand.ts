f6289bf9f4dd1ea52f35a46e76c088a5f17ab865
ANF-Studios <theanfstudios@gmail.com>
2021-02-05 12:33:51 -0500

    Refactored code

--- src/commands/core/HelpCommand.ts
+++ src/commands/core/HelpCommand.ts
@@ -17,45 +17,29 @@ export default class HelpCommand extends BaseCommand {
             color: EmbedColor.NULL,
             title: 'Help Command'
         });
-        if (args.includes("Utility")
-            || args.includes("Moderation")
-            || args.includes("User")
-            || "Custom Commands") {
-
-        } else {
-            for (let item in Categories) {
-                switch (item) {
-                    case Categories.Utility:
-                        helpEmbed.addField(item, 'Utilities for the bot', false);
-                        break;
-                    case Categories.User:
-                        helpEmbed.addField(item, 'User commands for them to customize', false);
-                        break;
-                    case Categories.Moderation:
-                        helpEmbed.addField(item, 'Moderation Commands', false);
-                        break;
-                    case Categories.CustomCommands:
-                        helpEmbed.addField(item, 'Custom user commands', false);
-                        break;
-                }
-            }
-            BaseCommand.commandInfo.forEach((value, index) => {
-                // This part is fine but as more commands get added,
-                // the embed amount needs to be limited, perhaps
-                // somewhere around 5 commands per embed and
-                // I think we should do a reaction embed
-                // as it has been requested a lot of times.
-                if (!BaseCommand.commandInfo[index][2]  // If the command isn't hidden,
-                    && embedFields < 5) { // and the fields number in the current embed is less than 5.
-                    helpEmbed.addField(value[0], // The name of the command.
-                        value[1], // The description of the command.
-                        false); // This is the inline value; for the discord embed.
-                    ++embedFields; // Increment this value.
-                }
-                logger.log(`${embedFields}`, logLevel.debug, 'HelpCommand')
-            });
+        // if (args.includes("Utility")
+        //     || args.includes("Moderation")
+        //     || args.includes("User")
+        //     || "Custom Commands") {
+        switch (args[0]) {
+            case "Utils":
+            case "Utility":
+            case "Utilities":
+                break;
         }
-        message.channel.send(helpEmbed);
+        if (message.member?.roles.cache.has("Administrator") // If the user has the Admin role
+            || message.member?.roles.cache.has("Staff") // Or the staff role
+            || message.author.id == message.guild?.ownerID) // Or even is the owner of the server
+            helpEmbed.addField(Categories[1][0], Categories[1][1]); // Add the moderation module.
+        Categories.forEach((element, index) => {
+            if (index !== 1) helpEmbed.addField(element[0], element[1]);
+        });
+        helpEmbed.title = "Here are modules that you can access!";
+        await message.channel.send(helpEmbed);
+    }
+
+    private static isAdmin(data: Message): boolean {
+        return data.author.id == data.guild?.ownerID;
     }
 }
 
@@ -67,3 +51,21 @@ export default class HelpCommand extends BaseCommand {
           false); // This is the inline value; for the discord embed.
         ++embedFields; // Increment this value.
 */
+
+/*
+        BaseCommand.commandInfo.forEach((value, index) => {
+            // This part is fine but as more commands get added,
+            // the embed amount needs to be limited, perhaps
+            // somewhere around 5 commands per embed and
+            // I think we should do a reaction embed
+            // as it has been requested a lot of times.
+            if (!BaseCommand.commandInfo[index][2]  // If the command isn't hidden,
+                && embedFields < 5) { // and the fields number in the current embed is less than 5.
+                helpEmbed.addField(value[0], // The name of the command.
+                    value[1], // The description of the command.
+                    false); // This is the inline value; for the discord embed.
+                ++embedFields; // Increment this value.
+            }
+            logger.log(`${embedFields}`, logLevel.debug, 'HelpCommand')
+        });
+ */
